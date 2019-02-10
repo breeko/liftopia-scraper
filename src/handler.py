126abc608ed7430439cbda0641cfeff3c5910639
breeko <branko.blagojevic@gmail.com>
2019-02-09 22:25:58 -0500

    update handler

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,11 @@
-from scrape_mountains import scrape_mountains
+from scrape_mountains import scrape_mountains_dynamic
 
 def handle(event, context):
-    mountains = event.get("mountains")
-    return scrape_mountains(mountains)
+    mountains = event.get("mountains") or event["multiValueQueryStringParameters"]["mountains"]
+    dynamic = scrape_mountains_dynamic(mountains)
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": { },
+        "body": str(dynamic)
+    }
